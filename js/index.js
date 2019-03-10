f8174758bc42a8d96393a7c8e5cbb423cf9c8381
WiltencirDG <wiltencir@gmail.com>
2019-03-10 03:20:50 -0300

    Initial cleanup

--- js/index.js
+++
@@ -1,8 +0,0 @@
-$("button#passbtn").on("click",function(){
-    setTimeout(function(){ window.location.assign("php/Login.php") }, 2000);
-});
-
-$("button#motobtn").on("click",function(){
-    setTimeout(function(){ window.location.assign("php/Login.php") }, 2000);
-});
-
