d5c183424269cdfe03c9b04f4791b779787a18c1
WiltencirDG <wiltencir@gmail.com>
2019-03-14 17:16:49 -0300

    Starting up

--- php/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define ('HOST','127.0.0.1'); 
-define ('USUARIO','root');
-define ('SENHA','brubo');
-define ('DB','Login');
-
-$conexao = myslqi_connect(HOST, USUARIO, SENHA, DB) or die ('SEM CONEXÃO COM O BANCO');
-?>
